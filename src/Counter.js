53117fb3d01dbfc2f5b498a25345f4926100ed9c
Artem <aprricod@gmail.com>
2021-06-26 10:06:46 +0300

    counter работает

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             counter: this.props.start || 0
+        }
+        this.handleButton = this.handleButton.bind(this)
+    }
+
+    handleButton(e) {
+        this.setState({counter: this.state.counter + this.props.step})
+    }
+    
+    render() {
+        return (
+            <>
+            {this.state.counter}
+                <button onClick={this.handleButton}>Увеличить значение</button>
+            </>
+        )
+    }
+}
